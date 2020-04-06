9b6fd1c6b00367867b9885e2cec2a3ef34d4214b
zohre <zohrefarzam@gmail.com>
2020-04-06 15:01:01 -0700

    cm

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import Dashboard from  '../views/Dashboard/Dashboard';
+import Dashboard from '../layouts/Dashboard/Dashboard';
 const indexRoutes = [{ path: "/", component: Dashboard }];
 
 export default indexRoutes;
